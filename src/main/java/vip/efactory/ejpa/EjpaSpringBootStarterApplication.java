f1229dcb91c86cd24e54f7137b6e5722b5ebfca9
dbdu <dusuanyun@sina.com>
2019-07-09 12:06:00 +0800

    第一次提交starter的代码

---
+++ src/main/java/vip/efactory/ejpa/EjpaSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+//package vip.efactory.ejpa;
+//
+//import org.springframework.boot.SpringApplication;
+////import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+////@SpringBootApplication
+//public class EjpaSpringBootStarterApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(EjpaSpringBootStarterApplication.class, args);
+//    }
+//
+//}
